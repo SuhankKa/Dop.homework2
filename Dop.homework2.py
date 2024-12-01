c999bf2d0702179b2d07c52c235064698014432c
Anastasiia <nastya21622162@gmail.com>
2024-12-01 22:11:08 +0300

    Add files via upload

---
+++ Dop.homework2.py
@@ -0,0 +1,25 @@
+# "Слишком древний шифр"
+
+a = int(input('Введи число от 3 до 20: '))
+password = []
+
+for i in range (1, 20):
+    big = True
+    for j in range (1, 20):
+        if a > 20:
+            big = False
+            break
+        k = j + 1
+        if i == k:
+            continue
+      
+        if a % (i+k) == 0 and i < k < 20:
+            password.append(i)
+            password.append(k)
+           
+if big:
+    print(password)
+    
+else:
+    print("Ошибка! Введи число от 3 до 20!")
+
